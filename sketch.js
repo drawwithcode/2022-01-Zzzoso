2e15240e92d04a888b1d031e3c5ad801e9505191
Francesco <fra.fatini1@gmail.com>
2023-01-28 15:51:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
-function preload() {
-  // put preload code here
-}
+let angle1 = 0; //angolo che fa l'espansione
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  angle1 = angle1 + 1; //angolo che fa l'espansione
+  let esp = abs(sin(angle1)) * 30; //larghezza dell'espansione
+  background(0, 0, 0, 60);
+
+  translate(width / 2, height / 2);
+  for (let i = 0; i < esp; i = i + 0.1) {
+    const angle = 5 * i + frameCount;
+    const rotation = sin(angle) * 900;
+    const ellWidth = 50 * i;
+    const ellHeight = 40 * i;
+
+    push();
+
+    rotate(rotation);
+    noFill();
+    stroke(255, 0, 0, 240);
+    strokeWeight(0.2);
+    ellipse(0, 0, ellWidth, ellHeight);
+    pop();
+  }
 }
